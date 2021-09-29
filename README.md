36c1db1647b18446bbee8e8d0414dbd8f275870e
Aaron Donelson <aaron.donelson23@gmail.com>
2021-09-29 10:02:47 -0400

    files were in the wrong folders and got rid of duplicates

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# node_readme
+
+    #Title:
+     Node readme creator
+    ##Description:
+    I was motivated to make this project to help simplify the process of making a readme doc
+    This project was made as a time saver to help free up essential time. 
+    This project solves a very needed problem of mine where I spend to much time creating a new readme doc everytime I make a new repo on github
+    I learned a significant amount about node.js and inquirer when working on this project
+    ##Installation:
+    To install and use this project you will need node.js and inquirer npm in order to get this to work
+    ##Usage:
+    Once inquirer and node.js are installed open VScode and right click, open terminal. type node index.js and the questions will launch. Simply answer the questions.
+    ##Credits:
+    For this project I used Udemy.com and https://www.npmjs.com/package/inquirer to reasearch how to accomplish this task. 
+    ##License:
+    - MIT License
+
+Copyright (c) [2021] [Aaron Donelson]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    ##Badges:
+    ![alt text](https://img.shields.io/badge/Index-HTML-yellowgreen)
+![alt text](https://img.shields.io/badge/Style-CSS-blue)
+![alt text](https://img.shields.io/badge/Script-JS-brightgreen)
+![alt text](https://img.shields.io/badge/JQuery-JQuery-orange)
--- index.js
+++ index.js
@@ -6,32 +6,106 @@ inquirer
         {
             type: 'input',
             message: "Whats the title of the project?",
-            name: 'title'           
+            name: 'title',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
         },
         {
             type: 'input',
-            message: "What was your motivation?",
-            name: 'Description'          
+            message: "What was your motivation? ",
+            name: 'Description',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}         
         },
         {
             type: 'input',
             message: "Why did you build this project?",
-            name: 'Description'           
+            name: 'Description1',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
         },
         {
             type: 'input',
             message: "What problem does it solve?",
-            name: 'Description'           
+            name: 'Description2',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
         },
         {
             type: 'input',
             message: "What did you learn?",
-            name: 'Description'           
+            name: 'Description3',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
         },
         {
             type: 'input',
-            message: "Whats the title of the project",
-            name: 'title'           
+            message: "What are the steps required to install your project?",
+            name: 'Installation',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
         },
+        {
+            type: 'input',
+            message: "Provide instructions and examples for use.",
+            name: 'Usage',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
+        },
+        {
+            type: 'input',
+            message: "List your collaborators",
+            name: 'Credits',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
+        },
+        {
+            type: 'input',
+            message: "Did you use third party assets for this project?",
+            name: 'Credits',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}},        
+        },
+        {
+            type: 'editor',
+            message: "Enter your license here",
+            name: 'License',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
+        },
+        {
+            type: 'editor',
+            message: "Do you want to input any badges?",
+            name: 'Badges',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
+        },
+        
     ]
+
 )
+.then(({
+    title,
+    Description,
+    Description1,
+    Description2,
+    Description3,
+    Installation,
+    Usage,
+    Credits,
+    License,
+    Badges
+}) =>{
+    const template = `
+    #Title:
+     ${title}
+    ##Description:
+    ${Description}
+    ${Description1}
+    ${Description2}
+    ${Description3}
+    ##Installation:
+    ${Installation}
+    ##Usage:
+    ${Usage}
+    ##Credits:
+    ${Credits}
+    ##License:
+    ${License}
+    ##Badges:
+    ${Badges}`;
+
+    fs.writeFile('readme.md', template, function(err){
+        if(err) throw err;
+    });
+    
+})
