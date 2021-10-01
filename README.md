09290dac70088c69ee9c4041d84a681fd784808c
Aaron Donelson <aaron.donelson23@gmail.com>
2021-09-30 20:09:26 -0400

    added google drive link in read me for video

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
     ##Installation:
     To install and use this project you will need node.js and inquirer npm in order to get this to work
     ##Usage:
-    Once inquirer and node.js are installed open VScode and right click, open terminal. Type "node index.js" press enter and the questions will launch. Simply answer the questions. If you would like to have the file saved somewhere other than my chosen path you must put that in on line 107.
+    Once inquirer and node.js are installed open VScode and right click, open terminal. Type "node index.js" press enter and the questions will launch. Simply answer the questions. If you would like to have the file saved somewhere other than my chosen path you must put that in on line 107. https://drive.google.com/file/d/1dnJ3p6KN6bkZASs3166vwsarYCVns7Mn/view
     ##Credits:
     For this project I used Udemy.com and https://www.npmjs.com/package/inquirer to reasearch how to accomplish this task. 
     ##License:
@@ -23,7 +23,7 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     ##Badges:
-    ![alt text](https://img.shields.io/badge/Index-HTML-yellowgreen)
+![alt text](https://img.shields.io/badge/Index-HTML-yellowgreen)
 ![alt text](https://img.shields.io/badge/Style-CSS-blue)
 ![alt text](https://img.shields.io/badge/Script-JS-brightgreen)
 ![alt text](https://img.shields.io/badge/JQuery-JQuery-orange)
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const chalk = require('chalk');
 
 inquirer
 .prompt([
@@ -58,16 +59,32 @@ inquirer
             validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}},        
         },
         {
-            type: 'editor',
+            type: 'checkbox',
             message: "Enter your license here",
             name: 'License',
+            choices: ["![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)", "![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)", "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"],
+            default: ["![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"],
             validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
         },
         {
-            type: 'editor',
+            type: 'checkbox',
             message: "Do you want to input any badges?",
             name: 'Badges',
+            choices: ["![alt text](https://img.shields.io/badge/Index-HTML-yellowgreen)", "![alt text](https://img.shields.io/badge/Style-CSS-blue)", "![alt text](https://img.shields.io/badge/Script-JS-brightgreen)", "![alt text](https://img.shields.io/badge/JQuery-JQuery-orange)"],
+            default: ["![alt text](https://img.shields.io/badge/Script-JS-brightgreen)"],
             validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}}           
+        },
+        {
+            type: 'input',
+            message: "What is your GitHub username?",
+            name: 'Username',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}},        
+        },
+        {
+            type: 'input',
+            message: "Enter your email address",
+            name: 'Email',
+            validate: (value) =>{ if(value){return true} else {return 'I need you to enter something in this field'}},        
         },
         
     ]
@@ -83,26 +100,46 @@ inquirer
     Usage,
     Credits,
     License,
-    Badges
+    Badges,
+    Username,
+    Email
 }) =>{
     const template = `
-    #Title:
-    ${title}
-    ##Description:
-    ${Description}
-    ${Description1}
-    ${Description2}
-    ${Description3}
-    ##Installation:
-    ${Installation}
-    ##Usage:
-    ${Usage}
-    ##Credits:
-    ${Credits}
-    ##License:
-    ${License}
-    ##Badges:
-    ${Badges}`;
+${License}
+
+#Title:
+${title}
+
+##Description:
+${Description}
+${Description1}
+${Description2}
+${Description3}
+
+##Table of Contents:
+[Installation](#installation)
+[Usage](#usage)
+[Credits](#credits)
+[License](#license)
+
+##Installation:
+${Installation}
+
+##Usage:
+${Usage}
+
+##Credits:
+${Credits}
+
+##License:
+${License}
+
+##Badges:
+${Badges}
+
+##Questions:
+https://github.com/${Username}/node_readme
+${Email}`;
 
     fs.writeFile('./../../../../../Desktop/Readme from node/readme.md', template, function(err){
        err ? console.error(err) : console.log("Your File Has Been Created");
