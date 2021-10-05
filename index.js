bf9603b04ca6792915170b2d08a1a2acf812f73a
Aaron Donelson <aaron.donelson23@gmail.com>
2021-10-04 20:09:38 -0400

    added .md to readme generator.

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ ${Badges}
 https://github.com/${Username}/node_readme
 ${Email}`;
 
-    fs.writeFile('./generated readme/readme', template, function(err){
+    fs.writeFile('./generated_readme/readme.md', template, function(err){
        err ? console.error(err) : console.log("Your File Has Been Created");
     });
     
